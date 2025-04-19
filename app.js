58ada941fab037438924b9fdcb551b6c97aa38ac
aman52kwah <amankwaamichael52@gmail.com>
2025-04-19 13:52:37 +0100

    fix Modal UI

--- app.js
+++ app.js
@@ -13,6 +13,56 @@ const completeElement = document.getElementById('complete')
 const completeElementInfo = document.getElementById('complete-info');
 const completeBtn = document.getElementById('complete-button');
 
+// modal dom
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal')
+const modalClose = document.getElementById('close-modal');
+
+
+const suggestedListNodeList = document.getElementsByClassName('suggested-list-li');
+for (let i = 0; i < suggestedListNodeList.length; i++){
+suggestedListNodeList[i].addEventListener('click',(e)=>{
+    modal.classList.remove('show-modal');
+    let value = e.target.innerHTML;
+    localStorage.removeItem('countdown');
+    countdownTitle = '';
+    countdownDate = '';
+    updateCountdown(value,suggestedList[value]);
+});
+}
+
+
+const suggestedList = {
+ 'Presidential Election': 1604361600000,
+  Christmas: 1608854400000,
+  Hanukkah: 1607558400000,
+  Thanksgiving: 1606348800000,
+  'Yom Kippur': 1601164800000,
+  "Valentine's Day": 1613260800000,
+  Halloween: 1604102400000,
+  'Friday the 13th': 1605225600000,
+  'Black Friday': 1606435200000,
+  "New Year's Eve": 1609372800000,
+};
+
+//show modal, focus on input
+function showModal(){
+    modal.classList.add('show-modal');
+}
+ // modal event listeners
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click',()=>modal.classList.remove('show-modal'));
+window.addEventListener('click', (e) => (e.target === modal ?modal.classList.remove('show-modal'): false));
+
+
+
+
+
+
+
+
+
+
 // global variables
 let countdownTitle = '';
 let countdownDate = '';
